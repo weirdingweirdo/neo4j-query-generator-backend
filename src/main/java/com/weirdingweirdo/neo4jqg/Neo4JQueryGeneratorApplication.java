e743af5fa180f3aaa872e3b3dfabae28d0747689
weirdingweirdo <whuang1118@gmail.com>
2018-05-09 21:47:10 -0400

    Initialized spring project

---
+++ src/main/java/com/weirdingweirdo/neo4jqg/Neo4JQueryGeneratorApplication.java
@@ -0,0 +1,12 @@
+package com.weirdingweirdo.neo4jqg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Neo4JQueryGeneratorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Neo4JQueryGeneratorApplication.class, args);
+	}
+}
---
+++ src/test/java/com/weirdingweirdo/neo4jqg/Neo4JQueryGeneratorApplicationTests.java
@@ -0,0 +1,16 @@
+package com.weirdingweirdo.neo4jqg;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Neo4JQueryGeneratorApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
